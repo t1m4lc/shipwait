1522506a5e69a92aa6de7c1a8f71471920e248a3
Timothy Alcaide <timothy.alcaide@gmail.com>
2025-05-10 18:51:12 +0200

    add dashboard setup

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <Button>Click me</Button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <SidebarProvider>
+      <LayoutAppSidebar />
+      <SidebarInset>
+        <LayoutHeader />
+        <div class="min-w-0 w-full flex-1 overflow-x-auto p-4 lg:p-6 pt-0 lg:pt-0">
+          <slot />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  </template>
+
+
