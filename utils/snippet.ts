09d64970c6a8d28b9918bc92265941e256290f62
Timothy Alcaide <timothy.alcaide@gmail.com>
2025-05-16 20:34:38 +0200

    fix project id

--- utils/snippet.ts
+++ utils/snippet.ts
@@ -3,5 +3,5 @@ import { SHIPWAIT_SNIPPET_CDN_URL } from "~/stores/constants";
 export default function generateSnippet(projectId: string) {
   const cdn = `${SHIPWAIT_SNIPPET_CDN_URL}`;
 
-  return `<script defer src="${cdn}" data-shipwait-id="${projectId}"></script>`;
+  return `<script defer src="${cdn}?id=${projectId}"></script>`;
 }
