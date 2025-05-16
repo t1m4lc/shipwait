e96dabd1da02b9e87cff2f63c70c1f45c549ffe1
Timothy Alcaide <timothy.alcaide@gmail.com>
2025-05-16 16:01:58 +0200

    update cdn url

--- stores/constants.ts
+++ stores/constants.ts
@@ -1,6 +1,5 @@
 export const SOCIAL_X = "https://x.com/t1m4lc";
 export const INSIGHTO = "https://bit.ly/4j3ZigM";
 
-export const SHIPWAIT_SNIPPET_VERSION = "latest";
-export const SHIPWAIT_SNIPPET_CDN =
-  "https://cdn.jsdelivr.net/gh/t1m4lc/shipwait-script";
+export const SHIPWAIT_SNIPPET_CDN_URL =
+  "https://cdn.jsdelivr.net/gh/t1m4lc/shipwait-script-dist@main/main.js";
--- utils/snippet.ts
+++ utils/snippet.ts
@@ -1,9 +1,11 @@
+import { SHIPWAIT_SNIPPET_CDN_URL } from "~/stores/constants";
+
 export default function generateSnippet(
   projectId: string,
   behaviour_type?: string,
   message?: string
 ) {
-  const cdn = `${SHIPWAIT_SNIPPET_CDN}@${SHIPWAIT_SNIPPET_VERSION}/dist/main.js?projectId=${projectId}&ty=${behaviour_type}&payload=${encodeURIComponent(
+  const cdn = `${SHIPWAIT_SNIPPET_CDN_URL}?projectId=${projectId}&ty=${behaviour_type}&payload=${encodeURIComponent(
     message || ""
   )}`;
 
