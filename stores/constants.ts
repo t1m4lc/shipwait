339ed2bcd178e98de0a840e3792030256381caa0
Timothy Alcaide <timothy.alcaide@gmail.com>
2025-06-23 15:42:16 +0200

    fix: pricing in test and live mode

--- stores/constants.ts
+++ stores/constants.ts
@@ -5,9 +5,19 @@ export const CONTACT_EMAIL = "timothy.alcaide@gmail.com";
 export const SHIPWAIT_SNIPPET_CDN_URL =
   "https://cdn.jsdelivr.net/gh/t1m4lc/shipwait-script-dist@v1.0.3/main.js";
 
-export const STRIPE_MONTHLY_PRICE_ID = "price_1Rd8PcAO7Z9aERoyahvlULIv";
-export const STRIPE_YEARLY_PRICE_ID = "price_1Rd8P5AO7Z9aERoyRbPSGXjL";
-export const STRIPE_LIFETIME_PRICE_ID = "price_1Rd8QDAO7Z9aERoy6Pe2KGls";
+const isProduction = process.env.NODE_ENV === "production";
+
+export const STRIPE_MONTHLY_PRICE_ID = isProduction
+  ? "price_1Rd8PcAO7Z9aERoyahvlULIv" // live mode
+  : "price_1Rbd4ZAO7Z9aERoyb0Y4iEGx"; // test mode
+
+export const STRIPE_YEARLY_PRICE_ID = isProduction
+  ? "price_1Rd8P5AO7Z9aERoyRbPSGXjL" // live mode
+  : "price_1Rbd8qAO7Z9aERoyJzV2JUfx"; // test mode
+
+export const STRIPE_LIFETIME_PRICE_ID = isProduction
+  ? "price_1Rd8QDAO7Z9aERoy6Pe2KGls" // live mode
+  : "price_1RbdSDAO7Z9aERoyLc8ZklJk"; // test mode
 
 export const FEATURE_FLAGS = {
   PROJECT_LIMIT: "project_limit",
